b40fe00ba9855a6f399096e9dcd72ef8acce5d0e
kavos113 <kohei.hadano@gmail.com>
2025-09-04 14:53:45 +0900

    elf loader

--- os/my-mikanos/MikanLoaderPkg/Main.c
+++ os/my-mikanos/MikanLoaderPkg/Main.c
@@ -2,11 +2,11 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
-#include  <Protocol/DiskIo2.h>
-#include  <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
+#include "elf.h"
 
 struct MemoryMap
 {
@@ -135,6 +135,41 @@ EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root)
     return EFI_SUCCESS;
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last)
+{
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    *first = MAX_UINT64;
+    *last = 0;
+
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i)
+    {
+        if (phdr[i].p_type != PT_LOAD) continue;
+
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr)
+{
+    Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i)
+    {
+        if (phdr[i].p_type != PT_LOAD) continue;
+
+        UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+        CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
+void Halt(void)
+{
+    while (1) __asm__("hlt");
+}
+
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE* system_table
@@ -167,34 +202,64 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
-    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+    VOID* kernel_buffer;
+    EFI_STATUS status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to allocate pool: %r\n", status);
+        Halt();
+    }
+    status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to read kernel file: %r\n", status);
+        Halt();
+    }
 
+    Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
 
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
-    typedef void EntryPointType(void);
-    EntryPointType* entry_point = (EntryPointType*)entry_addr;
-    Print(L"Kernel Entry Point: 0x%0lx", entry_point);
+    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to allocate pages: %r\n", status);
+        Halt();
+    }
 
-    EFI_STATUS status = gBS->ExitBootServices(image_handle, memmap.map_key);
+    CopyLoadSegments(kernel_ehdr);
+    Print(L"kernel: first addr = %08lx, last addr = %08lx\n", kernel_first_addr, kernel_last_addr);
+
+    status = gBS->FreePool(kernel_buffer);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to free pool: %r\n", status);
+        Halt();
+    }
+
+    status = gBS->ExitBootServices(image_handle, memmap.map_key);
     if (EFI_ERROR(status))
     {
         status = GetMemoryMap(&memmap);
         if (EFI_ERROR(status))
         {
             Print(L"failed to get memory map: %r\n", status);
-            while (1);
+            Halt();
         }
 
         status = gBS->ExitBootServices(image_handle, memmap.map_key);
         if (EFI_ERROR(status))
         {
             Print(L"could not exit boot services: %r\n", status);
-            while (1);
+            Halt();
         }
     }
+
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
+
+    typedef void EntryPointType(void);
+    EntryPointType* entry_point = (EntryPointType*)entry_addr;
     entry_point();
 
     Print(L"All done.");
