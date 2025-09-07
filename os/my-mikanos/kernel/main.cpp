df72ee2e6b0d31025f9f40fc046ef9dd1e1bf4bf
kavos113 <kohei.hadano@gmail.com>
2025-09-08 08:48:13 +0900

    update kernel

--- os/my-mikanos/kernel/main.cpp
+++ os/my-mikanos/kernel/main.cpp
@@ -1,4 +1,15 @@
-extern "C" void KernelMain()
+#include <cstdint>
+
+extern "C" void KernelMain(
+    uint64_t frame_buffer_base,
+    uint64_t frame_buffer_size
+)
 {
-    while (1) __asm__("hlt");
+    auto frame_buffer = reinterpret_cast<uint8_t*>(frame_buffer_base);
+    for (uint64_t i = 0; i < frame_buffer_size; ++i)
+    {
+        frame_buffer[i] = i % 256;
+    }
+
+    while (true) __asm__("hlt");
 }
