6ef8b8c594808053e0260c5a165d5a139b1e565e
kavos113 <kohei.hadano@gmail.com>
2025-09-08 15:16:21 +0900

    fix

--- os/my-mikanos/MikanLoaderPkg/Main.c
+++ os/my-mikanos/MikanLoaderPkg/Main.c
@@ -267,12 +267,6 @@ EFI_STATUS EFIAPI UefiMain(
         gop->Mode->FrameBufferSize
     );
 
-    UINT8* frame_buffer = (UINT8*)gop->Mode->FrameBufferBase;
-    for (UINTN i = 0; i < gop->Mode->FrameBufferSize; ++i)
-    {
-        frame_buffer[i] = 255;
-    }
-
     // open kernel file
     EFI_FILE_PROTOCOL* kernel_file;
     root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
