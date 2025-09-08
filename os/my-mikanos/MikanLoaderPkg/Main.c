3383139667a000f56b522782cee9ae2789aecbc8
kavos113 <kohei.hadano@gmail.com>
2025-09-08 15:13:57 +0900

    elf loader

--- os/my-mikanos/MikanLoaderPkg/Main.c
+++ os/my-mikanos/MikanLoaderPkg/Main.c
@@ -3,11 +3,13 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Guid/FileInfo.h>
 
 #include "frame_buffer_config.h"
+#include "elf.h"
 
 struct MemoryMap
 {
@@ -189,6 +191,42 @@ void Halt()
     }
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr *ehdr, UINT64 *first, UINT64 *last)
+{
+    Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64) ehdr + ehdr->e_phoff);
+    *first = MAX_UINT64;
+    *last = 0;
+
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i)
+    {
+        if (phdr[i].p_type != PT_LOAD)
+        {
+            continue;
+        }
+
+        *first = MIN(*first, phdr[i].p_vaddr);
+        *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+    }
+}
+
+void CopyLoadSegments(Elf64_Ehdr *ehdr)
+{
+    Elf64_Phdr *phdr = (Elf64_Phdr *)((UINT64) ehdr + ehdr->e_phoff);
+    for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i)
+    {
+        if (phdr[i].p_type != PT_LOAD)
+        {
+            continue;
+        }
+
+        UINT64 segm_in_file = (UINT64) ehdr + phdr[i].p_offset;
+        CopyMem((VOID *)phdr[i].p_vaddr, (VOID *)segm_in_file, phdr[i].p_filesz);
+
+        UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+        SetMem((VOID *)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+    }
+}
+
 EFI_STATUS EFIAPI UefiMain(
     EFI_HANDLE image_handle,
     EFI_SYSTEM_TABLE* system_table
@@ -196,6 +234,7 @@ EFI_STATUS EFIAPI UefiMain(
 {
     Print(L"Hello, Mikan World!\n");
 
+    // memmap
     CHAR8 memmap_buf[4096 * 4];
     struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
     GetMemoryMap(&memmap);
@@ -211,6 +250,7 @@ EFI_STATUS EFIAPI UefiMain(
     SaveMemoryMap(&memmap, memmap_file);
     memmap_file->Close(memmap_file);
 
+    // graphics output protocol
     EFI_GRAPHICS_OUTPUT_PROTOCOL* gop;
     OpenGOP(image_handle, &gop);
     Print(
@@ -233,6 +273,7 @@ EFI_STATUS EFIAPI UefiMain(
         frame_buffer[i] = 255;
     }
 
+    // open kernel file
     EFI_FILE_PROTOCOL* kernel_file;
     root_dir->Open(root_dir, &kernel_file, L"\\kernel.elf", EFI_FILE_MODE_READ, 0);
 
@@ -243,17 +284,43 @@ EFI_STATUS EFIAPI UefiMain(
     EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
     UINTN kernel_file_size = file_info->FileSize;
 
-    EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-    EFI_STATUS status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr);
+    VOID *kernel_buffer;
+    EFI_STATUS status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
     if (EFI_ERROR(status))
     {
-        Print(L"failed to allocate pages for kernel: %r\n", status);
+        Print(L"error: %r\n", status);
         Halt();
     }
-    kernel_file->Read(kernel_file, &kernel_file_size, (VOID*)kernel_base_addr);
-    Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+    status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
+    if (EFI_ERROR(status))
+    {
+        Print(L"error: %r\n", status);
+        Halt();
+    }
 
-    UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+    Elf64_Ehdr *kernel_ehdr = (Elf64_Ehdr *) kernel_buffer;
+    UINT64 kernel_first_addr, kernel_last_addr;
+    CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+    UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+    status = gBS->AllocatePages(AllocateAddress, EfiLoaderData, num_pages, &kernel_first_addr);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to allocate pages: %r\n", status);
+    }
+
+    CopyLoadSegments(kernel_ehdr);
+    Print(L"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+    status = gBS->FreePool(kernel_buffer);
+    if (EFI_ERROR(status))
+    {
+        Print(L"failed to free pool: %r\n", status);
+        Halt();
+    }
+
+    // call kernel entry point
+    UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
     struct FrameBufferConfig config = {
         (UINT8 *) gop->Mode->FrameBufferBase,
--- os/my-mikanos/kernel/elf.h
+++ os/my-mikanos/kernel/elf.h
@@ -41,4 +41,16 @@ typedef struct {
     Elf64_Xword	p_align;
 } Elf64_Phdr;
 
+#define PT_NULL 	0
+#define PT_LOAD 	1
+#define PT_DYNAMIC 	2
+#define PT_INTERP 	3
+#define PT_NOTE 	4
+#define PT_SHLIB 	5
+#define PT_PHDR 	6
+#define PT_TLS 	7
+#define PT_LOOS 	0x60000000
+#define PT_HIOS 	0x6fffffff
+#define PT_LOPROC 	0x70000000
+#define PT_HIPROC 	0x7fffffff
 #endif //KERNEL_ELF_H
