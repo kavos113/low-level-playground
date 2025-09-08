08af0f206fe232d50ffe9407360a5fbdab481e1c
kavos113 <kohei.hadano@gmail.com>
2025-09-08 09:07:04 +0900

    fix make

--- os/my-mikanos/kernel/main.cpp
+++ os/my-mikanos/kernel/main.cpp
@@ -1,4 +1,15 @@
-extern "C" void KernelMain()
+#include <cstdint>
+
+extern "C" void KernelMain(
+    uint64_t framebuffer_addr,
+    uint64_t framebuffer_size
+)
 {
+    auto framebuffer = reinterpret_cast<uint8_t*>(framebuffer_addr);
+    for (uint64_t i = 0; i < framebuffer_size; ++i)
+    {
+        framebuffer[i] = i % 256;
+    }
+
     while (true) __asm__("hlt");
 }
