1e4d59aa9e97863cc227c526419b3aa199063c81
kavos113 <kohei.hadano@gmail.com>
2025-09-08 10:28:20 +0900

    fb

---
+++ os/my-mikanos/kernel/frame_buffer_config.h
@@ -0,0 +1,21 @@
+#ifndef KERNEL_FRAME_BUFFER_CONFIG_H
+#define KERNEL_FRAME_BUFFER_CONFIG_H
+
+#include <cstdint>
+
+enum PixelFormat
+{
+    kPixelRGBResv8BitPerColor,
+    kPixelBGRResv8BitPerColor,
+};
+
+struct FrameBufferConfig
+{
+    uint8_t *frame_buffer;
+    uint32_t pixels_per_scan_line;
+    uint32_t horizontal_resolution;
+    uint32_t vertical_resolution;
+    PixelFormat pixel_format;
+};
+
+#endif //KERNEL_FRAME_BUFFER_CONFIG_H
--- os/my-mikanos/kernel/main.cpp
+++ os/my-mikanos/kernel/main.cpp
@@ -1,15 +1,57 @@
 #include <cstdint>
 
-extern "C" void KernelMain(
-    uint64_t framebuffer_addr,
-    uint64_t framebuffer_size
-)
+#include "frame_buffer_config.h"
+
+struct PixelColor
 {
-    auto framebuffer = reinterpret_cast<uint8_t*>(framebuffer_addr);
-    for (uint64_t i = 0; i < framebuffer_size; ++i)
+    uint8_t r, g, b;
+};
+
+int WritePixel(const FrameBufferConfig& config, int x, int y, const PixelColor& c)
+{
+    const int pixel_position = config.pixels_per_scan_line * y + x;
+
+    switch (config.pixel_format)
     {
-        framebuffer[i] = i % 256;
+    case kPixelRGBResv8BitPerColor:
+    {
+        uint8_t *p = &config.frame_buffer[4 * pixel_position];
+        p[0] = c.r;
+        p[1] = c.g;
+        p[2] = c.b;
+    }
+    case kPixelBGRResv8BitPerColor:
+    {
+        uint8_t *p = &config.frame_buffer[4 * pixel_position];
+        p[0] = c.b;
+        p[1] = c.g;
+        p[2] = c.r;
+    }
     }
 
-    while (true) __asm__("hlt");
+    return 0;
+}
+
+extern "C" void KernelMain(const FrameBufferConfig& config)
+{
+    for (int x = 0; x < config.horizontal_resolution; ++x)
+    {
+        for (int y = 0; y < config.vertical_resolution; ++y)
+        {
+            WritePixel(config, x, y, {255, 255, 255});
+        }
+    }
+
+    for (int x = 100; x < 300; ++x)
+    {
+        for (int y = 100; y < 200; ++y)
+        {
+            WritePixel(config, x, y, {0, 255, 0});
+        }
+    }
+
+    while (true)
+    {
+        __asm__("hlt");
+    }
 }
