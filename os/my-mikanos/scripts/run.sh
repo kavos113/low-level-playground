b9a2e7c791558d20e21da994852f11cc70c821bf
kavos113 <kohei.hadano@gmail.com>
2025-09-09 10:01:59 +0900

    draw a

--- os/my-mikanos/scripts/run.sh
+++ os/my-mikanos/scripts/run.sh
@@ -6,10 +6,14 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 cd "$EDK_DIR"
 source edksetup.sh
 if build; then
+    echo "---------------"
     echo "Build succeeded."
+    echo "---------------"
   "$SCRIPT_DIR"/qemu.sh Build/MikanLoaderX64/DEBUG_CLANG38/X64/Loader.efi "$SCRIPT_DIR"/../kernel/kernel.elf
 else
+    echo "---------------"
     echo "Build failed."
+    echo "---------------"
     exit 1
 fi
 
