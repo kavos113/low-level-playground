b934e9a2f862794e38822a6be1f98ae47ab75f1f
kavos113 <kohei.hadano@gmail.com>
2025-09-12 19:49:03 +0900

    printk

--- os/my-mikanos/kernel/main.cpp
+++ os/my-mikanos/kernel/main.cpp
@@ -1,12 +1,12 @@
 #include <cstdint>
 #include <cstddef>
 #include <cstdio>
+#include <cstdarg>
 
 #include "console.h"
 #include "font.h"
 #include "frame_buffer_config.h"
 #include "pixel_writer.h"
-
 
 void *operator new(size_t size, void *buf)
 {
@@ -20,6 +20,22 @@ void operator delete(void* obj) noexcept
 char pixel_writer_buf[sizeof(RGBPixelWriter)];
 PixelWriter *pixel_writer;
 
+char console_buf[sizeof(Console)];
+Console *console;
+
+int printk(const char *format, ...)
+{
+    va_list ap;
+    char s[1024];
+
+    va_start(ap, format);
+    int result = vsprintf(s, format, ap);
+    va_end(ap);
+
+    console->put_string(s);
+    return result;
+}
+
 extern "C" void KernelMain(const FrameBufferConfig *config)
 {
     switch (config->pixel_format)
@@ -32,6 +48,8 @@ extern "C" void KernelMain(const FrameBufferConfig *config)
         pixel_writer = new(pixel_writer_buf) BGRPixelWriter(config);
     }
 
+    console = new(console_buf) Console(pixel_writer, {255, 255, 255}, {0, 0, 0});
+
     for (int x = 0; x < config->horizontal_resolution; ++x)
     {
         for (int y = 0; y < config->vertical_resolution; ++y)
@@ -40,12 +58,9 @@ extern "C" void KernelMain(const FrameBufferConfig *config)
         }
     }
 
-    char buf[128];
-    Console console(pixel_writer, {255, 255, 255}, {0, 0, 0});
     for (int i = 0; i < 28; ++i)
     {
-        sprintf(buf, "line %d\n", i);
-        console.put_string(buf);
+        printk("printk: %d\n", i);
     }
 
     while (true)
